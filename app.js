58ce54b68172fdfe614e64c365bc4d9d4c1ae1ce
nireayoo <seminireayo@gmail.com>
2023-09-08 03:56:25 +0100

    new changes

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
-const app = express.Router();
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
 const PORT = 5000;
 
 app.listen(PORT, () =>{
